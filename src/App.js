202529acb0f333be5ed299ae421d10eb1b249b17
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-22 16:48:08 -0800

    set up App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+const users = [
+  { id: 'a', name: 'Robin' },
+  { id: 'b', name: 'Dennis' }
+];
+
+const App = () => {
+  const [text, setText] = React.useState('');
+  const [search, setSearch] = React.useState('');
+
+  const handleText = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSearch = () => {
+    setSearch(text);
+  };
+
+  const filteredUsers = users.filter((user) => {
+    return user.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={text} onChange={handleText} />
+      <button type="button" onClick={handleSearch}>
+        Search
+      </button>
+
+      <List list={filteredUsers} />
     </div>
   );
-}
+};
+
+const List = ({ list }) => {
+  return (
+    <ul>
+      {list.map((item) => (
+        <ListItem key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+const ListItem = ({ item }) => {
+  return <li>{item.name}</li>;
+};
 
 export default App;
